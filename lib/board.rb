d7eb3c047c9f7974201c261594f79698bd84ef11
Eli Fuchsman <efuchsman@gmail.com>
2022-08-31 20:26:16 -0600

    create method and test if a coordinate

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,7 @@ class Board
     def initialize
         @coordinate_keys = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
     end
-    
+
     def cells
         cells_hash = {}
         @coordinate_keys.each do |key|
@@ -12,5 +12,9 @@ class Board
         end
         cells_hash
     end
+
+    def valid_coordinate?(coordinate)
+        @coordinate_keys.include?(coordinate)
+      end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,29 +5,52 @@ require './lib/board.rb'
 require 'pry'
 
 RSpec.describe Board do
-    it 'exists' do
-        board = Board.new
+  it 'exists' do
+   board = Board.new
 
-        expect(board).to be_an_instance_of(Board)
+    expect(board).to be_an_instance_of(Board)
     end
 
-    describe 'cells' do
-        it 'returns a hash' do
-            board = Board.new
-            
-            expect(board.cells.class).to be Hash 
-        end
+  describe 'cells' do
+  it 'returns a hash' do
+    board = Board.new
 
-        it 'has 16 key/value pairs' do
-            board = Board.new
+    expect(board.cells.class).to be Hash
+  end
 
-            expect(board.cells.keys.count).to eq(16)
-        end
+  it 'has 16 key/value pairs' do
+    board = Board.new
 
-        it 'has cell objects' do 
-            board = Board.new
+    expect(board.cells.keys.count).to eq(16)
+  end
 
-            expect(board.cells["A1"]).to be_an_instance_of(Cell)
-        end
+  it 'has cell objects' do
+    board = Board.new
+    expect(board.cells["A1"]).to be_an_instance_of(Cell)
+  end
+  end
+
+  describe 'checks a coordinate' do
+  it 'checks if a coordinate is valid and will return true' do
+    board = Board.new
+
+    board.valid_coordinate?("A1")
+    board.valid_coordinate?("D4")
+
+    expect(board.valid_coordinate?("A1")).to be true
+    expect(board.valid_coordinate?("D4")).to be true
+
     end
+
+  it 'check is the coordinate is invalid and will return false' do
+    board = Board.new
+
+    board.valid_coordinate?("A5")
+
+    expect(board.valid_coordinate?("A5")).to be false
+  end
+  end
+
+
+
 end
