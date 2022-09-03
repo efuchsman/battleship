2f9fd5cb8afe37b883e0486b519308868750b991
Eli Fuchsman <efuchsman@gmail.com>
2022-09-03 15:03:56 -0600

    Create method for computer to select coordinates

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,34 @@ class Game
   end
 
 
+  def start
+    p "Welcome to BATTLESHIP"
+    p "Enter 'p' to play. Enter 'q' to quit."
 
+    user = gets.chomp.downcase
+
+    if user == 'q'
+    elsif user == 'p'
+      computer_place_ships(@computer.fleet, @computer.board.coordinate_keys)
+      play
+    else
+      'Please read the directions!'
+    end
+  end
+
+  def computer_place_ships(fleet, coordinates)
+    board = @computer.board
+    fleet.each do |ship|
+      selected_coords = coordinates.each_cons(ship.ship_length).to_a.sample
+      board.place(ship, selected_coords)
+    end
+    p @computer.board.render_player(true)
+  end
+
+  # def play
+  #   until player_1.has_lost? || computer.has_lost?
+
+  #   end
+  # end
 
 end
