b9061042d72ac63dd178a1afd4ca8b4bfa8fa5fb
Eli Fuchsman <efuchsman@gmail.com>
2022-09-03 15:36:27 -0600

    WIP player placing ships

--- lib/game.rb
+++ lib/game.rb
@@ -17,7 +17,13 @@ class Game
     if user == 'q'
     elsif user == 'p'
       computer_place_ships(@computer.fleet, @computer.board.coordinate_keys)
-      play
+      p "Enter the squares for the Cruiser (3 spaces):"
+      player_1_cruiser_coords = gets.chomp.upcase.split
+
+      @player_1.board.place(@player_1.fleet[0],player_1_cruiser_coords)
+
+      p @player_1.board.render_player(true)
+      # play
     else
       'Please read the directions!'
     end
@@ -29,7 +35,10 @@ class Game
       selected_coords = coordinates.each_cons(ship.ship_length).to_a.sample
       board.place(ship, selected_coords)
     end
-    p @computer.board.render_player(true)
+    p "I have laid out my ships on the grid."
+    p "You now need to lay out your two ships."
+    p "The Cruiser is three units long and the Submarine is two units long."
+    p @computer.board.render
   end
 
   # def play
