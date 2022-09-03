879db843b7ae1877b4aded6e8e524ab60031f6ca
Eli Fuchsman <efuchsman@gmail.com>
2022-09-03 13:59:45 -0600

    Create test to show board will render for p-layer

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,7 +9,9 @@ require 'pry-nav'
 RSpec.describe Player do
   it 'exists' do
     fleet = Ship.new('Cruiser', 3)
-   player = Player.new(fleet)
+    board = Board.new
+   player = Player.new(fleet, board)
+
 
     expect(player).to be_an_instance_of(Player)
   end
@@ -18,15 +20,31 @@ RSpec.describe Player do
       cruiser = Ship.new('Cruiser', 3)
       submarine = Ship.new('Submarine', 2)
       fleet = [cruiser, submarine]
-      player = Player.new(fleet)
+      board =Board.new
+      player = Player.new(fleet, board)
 
       expect(player.fleet).to eq([cruiser, submarine])
+    end
+
+    it 'can place its fleet on the board' do
+      cruiser = Ship.new('Cruiser', 3)
+      submarine = Ship.new('Submarine', 2)
+      fleet = [cruiser, submarine]
+      board =Board.new
+      player = Player.new(fleet, board)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+      board.place(submarine, ["C2", "D2"])
+
+      expect(player.board.render_player(true)).to eq("  1 2 3 4 \nA S S S • \nB • • • • \nC • S • • \nD • S • • \n")
+
     end
 
     it 'can check the health of the ships in the fleet' do
       cruiser = Ship.new('Cruiser', 3)
       submarine = Ship.new('Submarine', 2)
       fleet = [cruiser, submarine]
+      board = Board.new
       player = Player.new(fleet)
 
       expect(player.health_of_ship_at(0)).to eq(3)
@@ -60,5 +78,5 @@ RSpec.describe Player do
       expect(player.has_lost?).to be true
 
     end
-  end
+   end
 end
