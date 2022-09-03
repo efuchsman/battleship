bf8d08f4f68e603baf16c3c70609c760531a7226
Eli Fuchsman <efuchsman@gmail.com>
2022-09-03 16:12:27 -0600

    Create spec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,12 +13,31 @@ RSpec.describe Game do
     cruiser_2 = Ship.new('Cruiser', 3)
     submarine_1 = Ship.new('Submarine', 2)
     submarine_2 = Ship.new('Submarine', 2)
+    board_1 = Board.new
+    board_2 = Board.new
     fleet_1 = [cruiser_1, submarine_1]
     fleet_2 = [cruiser_2, submarine_2]
-    player_1 = Player.new(fleet_1)
-    computer = Player.new(fleet_2)
+    player_1 = Player.new(fleet_1, board_1)
+    computer = Player.new(fleet_2, board_2)
     game = Game.new(player_1, computer)
 
     expect(game).to be_an_instance_of(Game)
+  end
+
+  it 'plays has the computer place its fleet' do
+    cruiser_1 = Ship.new('Cruiser', 3)
+    cruiser_2 = Ship.new('Cruiser', 3)
+    submarine_1 = Ship.new('Submarine', 2)
+    submarine_2 = Ship.new('Submarine', 2)
+    board_1 = Board.new
+    board_2 = Board.new
+    fleet_1 = [cruiser_1, submarine_1]
+    fleet_2 = [cruiser_2, submarine_2]
+    player_1 = Player.new(fleet_1, board_1)
+    computer = Player.new(fleet_2, board_2)
+    game = Game.new(player_1, computer)
+
+    game.start
+
   end
 end
